d58bf7055b70117b9a16c88613e7bca037ffa230
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2022-06-22 12:48:32 +0300

    Resolve conversations after  review

--- server/server.js
+++ server/server.js
@@ -4,11 +4,8 @@ require('dotenv').config();
 
 const app = require('./app');
 
-try {
-  const EXPRESS_PORT = process.env.EXPRESS_PORT;
-  app.listen(EXPRESS_PORT, () => {
-    console.log(`Server listening on http://localhost:${EXPRESS_PORT}`);
-  });
-} catch (error) {
-  throw error;
-}
+const EXPRESS_PORT = process.env.EXPRESS_PORT || 8080;
+const HOST = process.env.HOST || 'localhost';
+app.listen(EXPRESS_PORT, HOST, () => {
+  console.log(`Server listening on http://${HOST}:${EXPRESS_PORT}`);
+});
