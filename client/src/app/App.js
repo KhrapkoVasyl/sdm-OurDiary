78a5b38ba403dddef7f95b9a20fc9496017498e9
artemmatiushenko1 <artom.matyushenko@gmail.com>
2022-06-23 09:00:21 +0300

    Created popup for creating and editing tasks

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,9 +1,30 @@
 import * as S from './App.style';
 import { Layout } from 'components/layout';
+import { Popup } from 'components/popup';
+import { AddTaskForm } from 'components/add-task-form';
+import { useState } from 'react';
 
 const App = () => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   return (
     <S.AppContainer>
+      <Popup
+        title={'Add new Task'}
+        openModal={openModal}
+        closeModal={closeModal}
+        isOpen={modalIsOpen}
+      >
+        <AddTaskForm />
+      </Popup>
       <Layout />
     </S.AppContainer>
   );
