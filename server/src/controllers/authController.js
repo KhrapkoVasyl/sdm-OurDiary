548e483b43618dd97193947a78a1993450517755
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2022-06-23 12:24:13 +0300

    Added login function for authentication controller

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -24,6 +24,28 @@ class AuthController {
       res.status(400).json({ message: err.message });
     }
   }
+
+  async login(req, res) {
+    try {
+      const { login, password } = req.body;
+
+      const User = findUserByNameService(login, password);
+      if (!User) {
+        throw new Error('User with such name does not exists!');
+      }
+
+      const isPasswordValid = comparePasswordsService(login, password); // сервіс comparePasswords
+      if (!isPasswordValid) {
+        throw new Error('Incorrect password');
+      }
+
+      const accessToken = generateAccessToken(User.id);
+
+      res.status(200).json({ message: 'Successfully logged in!', accessToken });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  }
 }
 
 module.exports = new AuthController();
