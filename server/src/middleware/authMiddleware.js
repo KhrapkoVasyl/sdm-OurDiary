cdee584179096a00ea963c883a2936ef8aeaf693
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2022-06-23 12:32:27 +0300

    Added authorization check middleware

---
+++ server/src/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function (req, res, next) {
+  if (req.method === 'OPTIONS') {
+    next();
+  }
+
+  try {
+    const accessToken = req.headers.accesstoken;
+
+    if (!accessToken) {
+      return res.status(401).json({ message: 'The user is not authorized' });
+    }
+
+    const accessTokenSecret = process.env.tokenSecret;
+    const payload = jwt.verify(accessToken, accessTokenSecret);
+    req.userId = payload.userId;
+
+    next();
+  } catch (err) {
+    if (err instanceof jwt.TokenExpiredError) {
+      return res.status(401).json({ message: 'The access token expired' });
+    }
+
+    if (err instanceof jwt.JsonWebTokenError) {
+      return res.status(401).json({ message: 'Invalid access token' });
+    }
+
+    return res.status(400).json({ message: err.message });
+  }
+};
