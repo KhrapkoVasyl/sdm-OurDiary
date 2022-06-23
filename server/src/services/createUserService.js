d4cd944349943206015a11f0669187f4dff55880
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2022-06-23 13:27:11 +0300

    Added create user service

---
+++ server/src/services/createUserService.js
@@ -0,0 +1,18 @@
+const db = require('../db/db');
+
+const createUserService = async (login, password) => {
+  const User = await db.findUserByName(login);
+  if (User) {
+    throw new Error('User with the same name is already exists!');
+  }
+
+  const newUser = await db.insertUser({ name: login, password });
+
+  if (!newUser) {
+    throw new Error('Failed to create User!');
+  }
+
+  return newUser.id;
+};
+
+module.exports = createUserService;
