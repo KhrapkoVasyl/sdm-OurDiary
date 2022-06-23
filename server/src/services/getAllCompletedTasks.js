c22bbaf2ea30f45256dc2c350ec223246af8c568
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2022-06-23 22:57:17 +0300

    Added get all completed user tasks service

---
+++ server/src/services/getAllCompletedTasks.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const db = require('../db/db');
+
+const getAllCompletedTasks = async (userID) => {
+  const user = db.findUserById(userID);
+  if (!user) {
+    throw new Error("User with this ID does not exists!")
+  }
+
+  const completedTasks = await db.findAllTasks({ userID, isDone: true });
+  if (completedTasks.length === 0) {
+    return [];
+  } else if (completedTasks.length > 1) {
+    completedTasks.sort((a, b) => a['deadline'] > b['deadline'] ? 1 : -1);
+  }
+
+  return completedTasks;
+};
+
+module.exports = getAllCompletedTasks;
