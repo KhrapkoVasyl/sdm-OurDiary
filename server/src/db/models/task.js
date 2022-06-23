614960fa3e6082ab76bc46c39d863884ea10e90d
VasylKhrapko <khrapko2002@gmail.com>
2022-06-23 14:10:53 +0300

    Added field deadline to task model

--- server/src/db/models/task.js
+++ server/src/db/models/task.js
@@ -5,7 +5,15 @@ const Model = require('./model');
 class Task extends Model {
   #requiredParams = ['id', 'userID', 'title', 'isDone'];
 
-  constructor({ id, userID, title, description, isDone, completionDate }) {
+  constructor({
+    id,
+    userID,
+    title,
+    description,
+    isDone,
+    completionDate,
+    deadline,
+  }) {
     super();
     this.id = id;
     this.userID = userID;
@@ -13,6 +21,7 @@ class Task extends Model {
     this.description = description || '';
     this.isDone = isDone;
     this.completionDate = completionDate || null;
+    this.deadline = deadline || null;
 
     this.checkRequiredParams(this.#requiredParams);
   }
