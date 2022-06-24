a6bf7b0e82fcd3bd9e34d7b788a3b8d20ef73b29
VasylKhrapko <khrapko2002@gmail.com>
2022-06-24 14:18:48 +0300

    Added function to convert string values to boolean

--- server/src/config.js
+++ server/src/config.js
@@ -3,9 +3,13 @@
 const ERR_CODE_NO_SUCH_FILE = 'ENOENT';
 const ERRNO_NO_SUCH_FILE = -4058;
 const PREFIX = '/api/v1';
+const FALSE_STR = 'false';
+const TRUE_STR = 'true';
 
 module.exports = {
   ERRNO_NO_SUCH_FILE,
   ERR_CODE_NO_SUCH_FILE,
   PREFIX,
+  FALSE_STR,
+  TRUE_STR,
 };
